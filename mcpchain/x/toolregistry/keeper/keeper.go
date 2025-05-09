83cdec8e2fce9a2574734f72e4664db60bec0cd8
Deeptanshu Sankhwar <desa4692@colorado.edu>
2025-05-08 22:52:02 -0600

    Add handlers to register and manage MCP servers on chain (#24)
    * scaffold registry module to manage mcp metadata on chain
    
    * Add handlers to register and manage an mcp server

--- mcpchain/x/toolregistry/keeper/keeper.go
+++ mcpchain/x/toolregistry/keeper/keeper.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"encoding/binary"
 	"fmt"
 
 	"cosmossdk.io/core/store"
@@ -10,6 +11,74 @@ import (
 
 	"mcpchain/x/toolregistry/types"
 )
+
+const (
+	ServerPrefix   = "RegisteredServer-value-"
+	ServerCountKey = "RegisteredServer-count"
+)
+
+func GetServerKey(id uint64) []byte {
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, id)
+	return bz
+}
+
+func (k Keeper) SetServer(ctx sdk.Context, server types.RegisteredServer) error {
+	store := k.storeService.OpenKVStore(ctx)
+	bz, err := k.cdc.Marshal(&server)
+	if err != nil {
+		return err
+	}
+	return store.Set(GetServerKey(server.Id), bz)
+}
+
+func (k Keeper) GetServer(ctx sdk.Context, id uint64) (types.RegisteredServer, bool) {
+	store := k.storeService.OpenKVStore(ctx)
+	bz, err := store.Get(GetServerKey(id))
+	if err != nil || bz == nil {
+		return types.RegisteredServer{}, false
+	}
+	var out types.RegisteredServer
+	if err := k.cdc.Unmarshal(bz, &out); err != nil {
+		return types.RegisteredServer{}, false
+	}
+	return out, true
+}
+
+func (k Keeper) DeleteServer(ctx sdk.Context, id uint64) error {
+	store := k.storeService.OpenKVStore(ctx)
+	return store.Delete(GetServerKey(id))
+}
+
+func (k Keeper) GetServerCount(ctx sdk.Context) uint64 {
+	store := k.storeService.OpenKVStore(ctx)
+	bz, err := store.Get([]byte(ServerCountKey))
+	if err != nil || bz == nil {
+		return 0
+	}
+	return binary.BigEndian.Uint64(bz)
+}
+
+func (k Keeper) SetServerCount(ctx sdk.Context, count uint64) error {
+	store := k.storeService.OpenKVStore(ctx)
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, count)
+	return store.Set([]byte(ServerCountKey), bz)
+}
+
+func (k Keeper) AppendRegisteredServer(ctx sdk.Context, server types.RegisteredServer) (uint64, error) {
+	id := k.GetServerCount(ctx)
+	server.Id = id
+
+	if err := k.SetServer(ctx, server); err != nil {
+		return 0, err
+	}
+	if err := k.SetServerCount(ctx, id+1); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
 
 type (
 	Keeper struct {
