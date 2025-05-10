6cd6090b8132adfd0f4aa7ad677db018363d94a0
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2025-05-09 20:52:12 -0600

    add keeper methods for attestation and define AttestationRecord proto

--- mcpchain/x/attestation/keeper/keeper.go
+++ mcpchain/x/attestation/keeper/keeper.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"encoding/binary"
 	"fmt"
 
 	"cosmossdk.io/core/store"
@@ -10,6 +11,74 @@ import (
 
 	"mcpchain/x/attestation/types"
 )
+
+const (
+	AttestationPrefix   = "AttestationRecord-value-"
+	AttestationCountKey = "AttestationRecord-count"
+)
+
+func GetAttestationKey(id uint64) []byte {
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, id)
+	return bz
+}
+
+func (k Keeper) SetAttestation(ctx sdk.Context, record types.AttestationRecord) error {
+	store := k.storeService.OpenKVStore(ctx)
+	bz, err := k.cdc.Marshal(&record)
+	if err != nil {
+		return err
+	}
+	return store.Set(GetAttestationKey(k.GetAttestationCount(ctx)), bz)
+}
+
+func (k Keeper) GetAttestationCount(ctx sdk.Context) uint64 {
+	store := k.storeService.OpenKVStore(ctx)
+	bz, err := store.Get([]byte(AttestationCountKey))
+	if err != nil || bz == nil {
+		return 0
+	}
+	return binary.BigEndian.Uint64(bz)
+}
+
+func (k Keeper) SetAttestationCount(ctx sdk.Context, count uint64) error {
+	store := k.storeService.OpenKVStore(ctx)
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, count)
+	return store.Set([]byte(AttestationCountKey), bz)
+}
+
+func (k Keeper) AppendAttestation(ctx sdk.Context, record types.AttestationRecord) (uint64, error) {
+	id := k.GetAttestationCount(ctx)
+
+	if err := k.SetAttestation(ctx, record); err != nil {
+		return 0, err
+	}
+	if err := k.SetAttestationCount(ctx, id+1); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (k Keeper) GetAllAttestations(ctx sdk.Context) []types.AttestationRecord {
+	store := k.storeService.OpenKVStore(ctx)
+	iterator, err := store.Iterator(nil, nil)
+	if err != nil {
+		return nil
+	}
+	defer iterator.Close()
+
+	var list []types.AttestationRecord
+	for ; iterator.Valid(); iterator.Next() {
+		var v types.AttestationRecord
+		if err := k.cdc.Unmarshal(iterator.Value(), &v); err != nil {
+			continue
+		}
+		list = append(list, v)
+	}
+	return list
+}
 
 type (
 	Keeper struct {
